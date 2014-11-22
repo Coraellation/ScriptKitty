2da108d2a0002df20e74559286f124d79bf56df1
Dian Jin <gryffindorstudent@gmail.com>
2014-11-21 22:13:20 -0500

    File reading functionality added
    now files from the sd card can be read. strings stored into arraylist

--- ScriptKitty/app/src/main/java/com/example/cora/scriptkitty/speech.java
+++ ScriptKitty/app/src/main/java/com/example/cora/scriptkitty/speech.java
@@ -2,34 +2,91 @@ package com.example.cora.scriptkitty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Locale;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class speech extends Activity {
 
     TextToSpeech ttobj;
     private EditText write;
+    private TextView txtDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech);
+        txtDisplay = (TextView)findViewById(R.id.editText1);
         write = (EditText)findViewById(R.id.editText1);
         ttobj=new TextToSpeech(getApplicationContext(),
-        new TextToSpeech.OnInitListener() {
-        @Override
-        public void onInit(int status) {
-            if(status != TextToSpeech.ERROR){
-                ttobj.setLanguage(Locale.UK);
-                }
+                new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status != TextToSpeech.ERROR){
+                            ttobj.setLanguage(Locale.UK);
+                        }
+                    }
+                });
+
+
+        // Global variables
+        String fileDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String fileName = "script.txt";
+        File myScript = new File(fileDir + File.separatorChar + fileName);
+        ArrayList<String> lineList;
+        lineList = readFile(myScript);
+        txtDisplay.setText(Integer.toString(lineList.size()));
+
+    }
+
+    public static ArrayList<String> readFile(File f) {
+        ArrayList<String> lineList = new ArrayList<String>();
+        lineList.add("ew");
+        if (f.isFile() && f.canRead()) {
+            // Initialize thefilereaders
+            FileReader fr = null;
+            BufferedReader br;
+            String lineThis;
+
+            // Initialize fileReader
+            try {
+                fr = new FileReader(f);
+            } catch (Exception e) {
+
             }
-        });
+            // Initialize bufferedReader
+            try {
+                br = new BufferedReader(fr);
+                while ((lineThis = br.readLine()) != null) {
+                    lineList.add(lineThis);
+                }
+                br.close();
+            }
+            catch (IOException e) {
+
+            }
+        } else {
+        }
+        return lineList;
     }
 
     @Override
